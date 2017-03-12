eefd922121ed588b65bf7d445856bffed0e62bb6
justolin <100302004@nccu.edu.tw>
2017-03-12 19:34:35 +0800

    Update hw1_104352015.R

--- hw1_104352015.R
+++ hw1_104352015.R
@@ -1,6 +1,3 @@
-########################
-#### homework1 code ####
-########################
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("USAGE: Rscript hw1_104352015.R input", call.=FALSE)
@@ -8,9 +5,7 @@ if (length(args)==0) {
   d <- read.csv(args[1])
   nums <- sapply(d, is.numeric)
   numd <- d[,nums]
-  ## summary maximum
   maxcol <- round(apply(numd,2,max),2)
   result <- rbind(c("set",colnames(numd)),c(args[1],maxcol))
-  ## summary output csv
   write.table(result,args[2],sep=",",row.names = FALSE,col.names = FALSE)
 }
