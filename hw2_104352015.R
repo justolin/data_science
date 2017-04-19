c2de45760a11325dc68e992ab8eb7cf4c87abe8c
justolin <100302004@nccu.edu.tw>
2017-04-19 22:45:43 +0800

    Update hw2_104352015.R

--- hw2_104352015.R
+++ hw2_104352015.R
@@ -1,13 +1,10 @@
 ##### function
-##### produce AUC simulation #####
-get_AUC <- function(pos.scores,neg.scores,n_sample=1000000) {
-  # Arguments:
-  #   pos.scores: vectors of positive observations' scores 
-  #   neg.scores: vectors of negative observations' scores 
-  #   n_samples : number of samples to approximate AUC
-  pos.sample <- sample(pos.scores, n_sample, replace=TRUE)
-  neg.sample <- sample(neg.scores, n_sample, replace=TRUE)
-  mean(1.0*(pos.sample > neg.sample) + 0.5*(pos.sample==neg.sample))
+##### produce AUC #####
+library('ROCR')
+get_AUC <- function(pred_score,reference) {
+  predict <- prediction(pred_score,reference)
+  auc <- attributes(performance(predict,'auc'))$y.values[[1]]
+  return (auc)
 }
 
 ##### read parameters #####
@@ -17,7 +14,7 @@ if (length(args)==0) {
 }
 
 ##### parse parameters #####
-i<-1 
+i<-1
 while(i < length(args))
 {
   if(args[i] == "--target"){
@@ -41,7 +38,6 @@ print(paste("query mode :",target))
 print(paste("output file:",out_f))
 print(paste("files:",files))
 
-files <- unique(files)
 if (target == "male"){
   target_n <- "female"
 }else if(target == "female"){
@@ -56,9 +52,7 @@ F1 <- c()
 AUC <- c()
 for(file in files)
 {
-  name <- gsub("meth","method",basename(file))
-  file_names <- paste(name,".csv",sep = "")
-  d <- read.table(file_names, header=TRUE , sep=",")
+  d <- read.table(file, header=TRUE , sep=",")
   TP <- length(which(d$prediction == target & d$reference == target))
   TN <- length(which(d$prediction == target_n & d$reference == target_n))
   FP <- length(which(d$prediction == target & d$reference == target_n))
@@ -66,25 +60,17 @@ for(file in files)
   sensitivity<-c(sensitivity, TP/(TP+FN))
   specificity<-c(specificity, TN/(TN+FP))
   F1 <- c(F1, 2*TP/(2*TP+FP+FN))
-  if (target == "male"){
-    pos <- subset(d,reference == target)[,4]
-    neg <- subset(d,reference == target_n)[,4]
-  }else if(target == "female"){
-    pos <- 1-subset(d,reference == target)[,4]
-    neg <- 1-subset(d,reference == target_n)[,4]
-  }
-  AUC <- c(AUC, get_AUC(pos,neg))
-  method<-c(method,name)
+  AUC <- c(AUC, get_AUC(d$pred.score, d$reference))
+  method <- c(method,gsub(".csv", "", basename(file)))
 }
 
 ##### results formated and output #####
 out_data<-data.frame(method = method, sensitivity = sensitivity, 
                      specificity = specificity,F1 = F1, AUC = AUC,
                      stringsAsFactors = FALSE)
-max_index <- apply(out_data[,2:5],2,which.max)
-max_method <- c("method",method[max_index])
-out_data <- out_data[rank(method),]
-out_data <- format(head(out_data), digits=2) 
+max_index <- apply(out_data[,-1],2,which.max)
+max_method <- c("highest",method[max_index])
+out_data <- format(out_data, digits=2)
 out_data <- rbind(out_data,max_method)
 if (grepl("csv",out_f) == TRUE){
   write.table(out_data,out_f, row.names = FALSE,sep=",")
